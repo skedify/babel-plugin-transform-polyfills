f946461f41793712cbffdcbb7631189abdee4cc6
ThomasB <thomas.buys45@gmail.com>
2018-01-15 12:40:54 +0100

    fix: check if window.Promise exists in Promise polyfills
    previously we checked if window.Promise.all (or resolve, ...) exists, but this throws an error when
    window.Promise is already undefined (IE11)

--- polyfills/Promise/race.js
+++ polyfills/Promise/race.js
@@ -1,5 +1,5 @@
 import PromisePolyfill from 'es6-promise'
 
-export default (Function.prototype.isPrototypeOf(window.Promise.race)
+export default (Function.prototype.isPrototypeOf(window.Promise)
   ? window.Promise.race.bind(window.Promise)
   : PromisePolyfill.race.bind(PromisePolyfill))
